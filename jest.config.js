4a7f894ed708caedbf32533b616340b1167cefa7
Niles Cabrera <nilestristan@gmail.com>
2026-04-05 02:58:47 +0800

    chore: exclude .worktrees from Jest test discovery

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ const config = {
     "^@/(.*)$": "<rootDir>/$1",
   },
   testMatch: ["**/__tests__/**/*.test.ts"],
+  testPathIgnorePatterns: ["/node_modules/", "/.worktrees/"],
 };
 
 module.exports = createJestConfig(config);
