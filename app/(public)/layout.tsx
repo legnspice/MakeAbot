90e99f62f63182f7c28ddcde7a5aa628a14fee11
Niles Cabrera <nilestristan@gmail.com>
2026-04-05 10:59:08 +0800

    fix: wire TutorialProvider and TutorialModal into public layout

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -4,9 +4,30 @@ import { useCurrentUser } from "@/hooks/use-current-user";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { AuthProvider } from "@/contexts/auth-context";
+import { TutorialProvider, useTutorial, TUTORIAL_STORAGE_KEY } from "@/contexts/tutorial-context";
+import { TutorialModal } from "@/components/tutorial-modal";
 import { PageShellSkeleton } from "@/components/ui/page-shell-skeleton";
 import { DisclaimerModal } from "@/components/disclaimer-modal";
 import { AppFooter } from "@/components/app-footer";
+
+function PublicContent({ children }: { children: React.ReactNode }) {
+  const { openTutorial } = useTutorial();
+
+  function handleDisclaimerAccept() {
+    if (!localStorage.getItem(TUTORIAL_STORAGE_KEY)) {
+      openTutorial();
+    }
+  }
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <DisclaimerModal onAccept={handleDisclaimerAccept} />
+      <TutorialModal />
+      <div className="flex-1">{children}</div>
+      <AppFooter />
+    </div>
+  );
+}
 
 export default function PublicLayout({
   children,
@@ -31,12 +52,10 @@ export default function PublicLayout({
   }
 
   return (
-    <AuthProvider userData={userData}>
-      <div className="flex flex-col min-h-screen">
-        <DisclaimerModal />
-        <div className="flex-1">{children}</div>
-        <AppFooter />
-      </div>
-    </AuthProvider>
+    <TutorialProvider>
+      <AuthProvider userData={userData}>
+        <PublicContent>{children}</PublicContent>
+      </AuthProvider>
+    </TutorialProvider>
   );
 }
